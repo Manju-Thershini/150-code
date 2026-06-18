698d48344d21709df786076bbc05537b6461039d
Manju Thershini S <manjuthershinis@gmail.com>
2026-06-18 16:23:16 +0000

    fourtheen

---
+++ t14.java
@@ -0,0 +1,30 @@
+//Number of Primes in a specified range: 
+import java.util.*;
+class t14
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int count=0,num=1; 
+       
+        while(count<n)
+        {          
+                num++;
+                boolean prime=true;
+                for(int i=2;i<=Math.sqrt(num);i++)
+                {
+                    if(num %i==0)
+                    {
+                        prime=false;
+                        break;
+                    }
+                }
+                if(prime==true){
+                count++;
+                System.out.println(num +" ,");
+                }
+        }
+        
+    }
+}
