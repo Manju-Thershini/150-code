84bb96eaa66435de42f2e6d52981211869b218c4
Manju Thershini S <manjuthershinis@gmail.com>
2026-06-18 04:10:36 +0000

    seven

---
+++ seven.java
@@ -0,0 +1,19 @@
+//ofgiven 5 numbers how many are even
+import java.util.*;
+class seven
+{
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=5,c=0;
+        int a[]=new  int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+            if(a[i]%2==0)
+            c++;
+
+        }
+        System.out.println("no.of even numbers "+c);
+    }
+}
