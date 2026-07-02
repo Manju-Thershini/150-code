eeede814856cd002d7495daf10ada73fdf4e7656
Manju Thershini S <manjuthershinis@gmail.com>
2026-07-02 18:18:25 +0000

    t28

---
+++ t28.java
@@ -0,0 +1,19 @@
+//digitSum: sum of all digits in N
+import java.util.*;
+class t28
+{
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the number :");
+        int  n =sc.nextInt();
+        int s=0;
+        while(n>0)
+        {
+            int a=n%10;
+            s+=a;
+            n=n/10;
+        }
+        System.out.println(s);
+    }
+}
