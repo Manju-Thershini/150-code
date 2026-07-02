8b18b4ecb4afd6823a1a87369d74d3c4153da3e8
Manju Thershini S <manjuthershinis@gmail.com>
2026-07-02 18:18:56 +0000

    t29

---
+++ t29.java
@@ -0,0 +1,28 @@
+//Non-Repeated Digits’ Count:
+import java.util.*;
+class t29
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number : ");
+        int n=sc.nextInt();
+        int a[]=new int[10];
+        int c=0;
+        while(n>0)
+        {
+            int r=n%10;
+            a[r]++;
+            n=n/10;
+        }
+        for(int num : a)
+        {
+            if(num==1)
+            {
+                c++;
+
+            }
+        }
+        System.out.println("No. of non-repeated digits : "+c);
+    }
+}
